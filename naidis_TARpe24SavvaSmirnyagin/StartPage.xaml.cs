fcbe674a08d69d08aa6d8d92e2000f3271c58214
savva <smirnyaginsavva@gmail.com>
2026-02-11 11:29:52 +0200

    tunnitöö tehtud

--- naidis_TARpe24SavvaSmirnyagin/StartPage.xaml.cs
+++ naidis_TARpe24SavvaSmirnyagin/StartPage.xaml.cs
@@ -2,8 +2,8 @@ namespace naidis_TARpe24SavvaSmirnyagin;
 
 public partial class StartPage : ContentPage
 {
-    public List<ContentPage> lehed = new List<ContentPage>() { new TextPage(0), new FigurePage(1) };
-    public List<string> tekstid = new List<string>() { "Tee lahti TextPage", "Tee lahti FigurePage"};
+    public List<ContentPage> lehed = new List<ContentPage>() { new TextPage(), new FigurePage() };
+    public List<string> leheNimed = new List<string>() { "Tekst", "Kujund"};
 
     ScrollView sv;
     VerticalStackLayout vst;
@@ -11,29 +11,28 @@ public partial class StartPage : ContentPage
     {
         //InitializeComponent();
         Title = "Avaleht";
-        vst = new VerticalStackLayout { BackgroundColor = Color.FromRgb(180, 100, 20) };
-        for (int i = 0, i < tekstid.Count; i++)
+        vst = new VerticalStackLayout { Padding = 20, Spacing = 15 };
+        for (int i = 0; i < lehed.Count; i++)
         {
             Button nupp = new Button
             {
-                Text = tekstid[i],
-                BackgroundColor = Color.FromRgb(20, 100, 200),
-                TextColor = Color.FromRgb(10, 20, 15),
-                FontFamily = "Regular.ttf",
-                BorderWidth = 10,
+                Text = leheNimed[i],
+                FontSize = 36,
+                FontFamily = "Luffio",
+                BackgroundColor = Colors.LightGray,
+                TextColor = Colors.Black,
+                CornerRadius = 10,
+                HeightRequest = 60,
                 ZIndex = i
             };
             vst.Add(nupp);
-            nupp.Clicked += Nupp_Clicked
+            nupp.Clicked += (sender, e) =>
+            {
+                var valik = lehed[nupp.ZIndex];
+                Navigation.PushAsync(valik);
+            };
         }
         sv = new ScrollView { Content = vst };
         Content = sv;
-    }
-
-    private async void Nupp_Clicked(object? sender, EventArgs e)
-    {
-        Button btn = sender as Button;
-        await Navigation.PushAsync(lehed[btn.ZIndex]);
-
     }
 }
