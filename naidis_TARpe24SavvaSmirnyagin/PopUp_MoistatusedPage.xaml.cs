1a9042eddff243997ddc7196561c2d6fac5463eb
savva <smirnyaginsavva@gmail.com>
2026-03-09 12:53:52 +0200

    moistatused page valmis

--- naidis_TARpe24SavvaSmirnyagin/PopUp_MoistatusedPage.xaml.cs
+++ naidis_TARpe24SavvaSmirnyagin/PopUp_MoistatusedPage.xaml.cs
@@ -8,8 +8,17 @@ public partial class PopUp_MoistatusedPage : ContentPage
     Label nimelabel;
     string nimi;
     string valik;
+    Button algusnupp;
     Button esimene;
+    Button teine;
+    Button kolmas;
+    Button neljas;
+    Button loppbutton;
     Label esimenelabel;
+    Label teinelabel;
+    Label kolmaslabel;
+    Label neljaslabel;
+    int count;
 	public PopUp_MoistatusedPage()
 	{
 
@@ -41,6 +50,34 @@ public partial class PopUp_MoistatusedPage : ContentPage
             HorizontalOptions = LayoutOptions.Center
         };
 
+        teinelabel = new Label
+        {
+            Text = "",
+            FontSize = 16,
+            FontFamily = "Times new roman",
+            TextColor = Colors.Black,
+            HorizontalOptions = LayoutOptions.Center
+        };
+
+        kolmaslabel = new Label
+        {
+            Text = "",
+            FontSize = 16,
+            FontFamily = "Times new roman",
+            TextColor = Colors.Black,
+            HorizontalOptions = LayoutOptions.Center
+        };
+
+        neljaslabel = new Label
+        {
+            Text = "",
+            FontSize = 16,
+            FontFamily = "Times new roman",
+            TextColor = Colors.Black,
+            HorizontalOptions = LayoutOptions.Center
+        };
+
+
         Button nulliNupp = new Button
         {
             Text = "Nulli seaded (Testimiseks)",
@@ -51,7 +88,7 @@ public partial class PopUp_MoistatusedPage : ContentPage
             Margin = new Thickness(0, 30, 0, 0)
         };
 
-        Button algusnupp = new Button
+        algusnupp = new Button
         {
             Text = "Alustuseks",
             BackgroundColor = Colors.Black,
@@ -72,6 +109,50 @@ public partial class PopUp_MoistatusedPage : ContentPage
         };
         esimene.Clicked += Esimene_Clicked;
 
+        teine = new Button
+        {
+            Text = "Teine mõistatus",
+            BackgroundColor = Colors.Black,
+            TextColor = Colors.White,
+            VerticalOptions = LayoutOptions.Start,
+            HorizontalOptions = LayoutOptions.Center,
+            IsVisible = false
+        };
+        teine.Clicked += Teine_Clicked;
+
+        kolmas = new Button
+        {
+            Text = "Kolmas mõistatus",
+            BackgroundColor = Colors.Black,
+            TextColor = Colors.White,
+            VerticalOptions = LayoutOptions.Start,
+            HorizontalOptions = LayoutOptions.Center,
+            IsVisible = false
+        };
+        kolmas.Clicked += Kolmas_Clicked;
+
+        neljas = new Button
+        {
+            Text = "Neljas mõistatus",
+            BackgroundColor = Colors.Black,
+            TextColor = Colors.White,
+            VerticalOptions = LayoutOptions.Start,
+            HorizontalOptions = LayoutOptions.Center,
+            IsVisible = false
+        };
+        neljas.Clicked += Neljas_Clicked;
+
+        loppbutton = new Button
+        {
+            Text = "Lõpetuseks",
+            BackgroundColor = Colors.Black,
+            TextColor = Colors.White,
+            VerticalOptions = LayoutOptions.Start,
+            HorizontalOptions = LayoutOptions.Center,
+            IsVisible = false
+        };
+        loppbutton.Clicked += Lopp_Clicked;
+
 
         vsl = new VerticalStackLayout
         {
@@ -79,7 +160,7 @@ public partial class PopUp_MoistatusedPage : ContentPage
             Spacing = 15,
             Children = 
             { 
-                pealkiri, algusnupp, nimelabel, esimene, esimenelabel 
+                pealkiri, algusnupp, nimelabel, esimene, esimenelabel, teine, teinelabel, kolmas, kolmaslabel, neljas, neljaslabel, loppbutton 
             },
             HorizontalOptions = LayoutOptions.Center
         };
@@ -94,6 +175,7 @@ public partial class PopUp_MoistatusedPage : ContentPage
         {
             nimelabel.Text = $"Tere, {nimi}!";
             esimene.IsVisible = true;
+            algusnupp.IsEnabled = false;
         }
         else
         {
@@ -107,15 +189,86 @@ public partial class PopUp_MoistatusedPage : ContentPage
 
         if (valik != null && valik != "Loobu" && valik == "Lumi")
         {
-            await DisplayAlertAsync("Vastus", "Valisid õige vastuse!", "OK");
+            await DisplayAlertAsync("Kontroll", "Valisid õige vastuse!", "OK");
             esimene.IsEnabled = false;
             esimenelabel.Text = "Õige!";
+            teine.IsVisible = true;
+            count++;
         }
         else if (valik != null && valik != "Loobu" && valik != "Lumi")
         {
-            await DisplayAlertAsync("Vastus", "Valisid vale vastuse!", "OK");
+            await DisplayAlertAsync("Kontroll", "Valisid vale vastuse!", "OK");
             esimene.IsEnabled = false;
             esimenelabel.Text = "Vale!";
+            teine.IsVisible = true;
         }
+    }
+
+    private async void Teine_Clicked(object? sender, EventArgs e)
+    {
+        valik = await DisplayActionSheet("Hommikul sünnib, õhtul sureb.", "Loobu", null, "Inimene", "Päike", "Lumi");
+
+        if (valik != null && valik != "Loobu" && valik == "Päike")
+        {
+            await DisplayAlertAsync("Kontroll", "Valisid õige vastuse!", "OK");
+            teine.IsEnabled = false;
+            teinelabel.Text = "Õige!";
+            kolmas.IsVisible = true;
+            count++;
+        }
+        else if (valik != null && valik != "Loobu" && valik != "Päike")
+        {
+            await DisplayAlertAsync("Kontroll", "Valisid vale vastuse!", "OK");
+            teine.IsEnabled = false;
+            teinelabel.Text = "Vale!";
+            kolmas.IsVisible = true;
+        }
+    }
+
+    private async void Kolmas_Clicked(object? sender, EventArgs e)
+    {
+        valik = await DisplayActionSheet("Ilma jaluta käib, ilma tiivuta lendab.", "Loobu", null, "Lind", "Pilv", "Lennuk");
+
+        if (valik != null && valik != "Loobu" && valik == "Pilv")
+        {
+            await DisplayAlertAsync("Kontroll", "Valisid õige vastuse!", "OK");
+            kolmas.IsEnabled = false;
+            kolmaslabel.Text = "Õige!";
+            neljas.IsVisible = true;
+            count++;
+        }
+        else if (valik != null && valik != "Loobu" && valik != "Pilv")
+        {
+            await DisplayAlertAsync("Kontroll", "Valisid vale vastuse!", "OK");
+            kolmas.IsEnabled = false;
+            kolmaslabel.Text = "Vale!";
+            neljas.IsVisible = true;
+        }
+    }
+
+    private async void Neljas_Clicked(object? sender, EventArgs e)
+    {
+        valik = await DisplayPromptAsync("Neljas mõistatus", "Ei ole toas ega õues?");
+
+        if (valik != null && valik != "Loobu" && valik == "aken" || valik == "Aken")
+        {
+            await DisplayAlertAsync("Kontroll", "Valisid õige vastuse!", "OK");
+            neljas.IsEnabled = false;
+            neljaslabel.Text = "Õige!";
+            count++;
+            loppbutton.IsVisible = true;
+        }
+        else if (valik != null && valik != "Loobu" && valik != "Pilv")
+        {
+            await DisplayAlertAsync("Kontroll", "Valisid vale vastuse!", "OK");
+            neljas.IsEnabled = false;
+            neljaslabel.Text = "Vale!";
+            loppbutton.IsVisible = true;
+        }
+    }
+
+    private async void Lopp_Clicked(object? sender, EventArgs e)
+    {
+        await DisplayAlertAsync("Tulemused", $"Vastasid {count}/4 mõistatusele õigesti!", "OK");
     }
 }
